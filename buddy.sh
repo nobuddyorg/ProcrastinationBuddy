fc35678fa5cbbb446fa674bf850d613f8f1af73a
Matthias Eggert <dem.matze@gmail.com>
2025-05-17 13:06:44 +0200

    buddy script

---
+++ buddy.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+case "$1" in
+  start)
+    echo "Checking for docker installation..."
+    if ! command -v docker &> /dev/null
+    then
+        echo "docker could not be found"
+        exit 1
+    fi
+
+    if ! docker info > /dev/null 2>&1
+    then
+        echo "docker is not running"
+        exit 1
+    fi
+
+    echo "Starting docker containers..."
+    docker compose pull
+    docker compose up --build --force-recreate --detach
+
+    echo "Waiting for Ollama to be ready..."
+    until curl -s http://localhost:11434 | grep -q "Ollama is running"; do
+      echo -n "."
+      sleep 1
+    done
+
+    echo "Downloading initial model (llama3:8b)..."
+    curl -s -X POST http://localhost:11434/api/pull -d '{"name": "llama3:8b"}'
+
+    echo -e "\033[0;32mAccess UI at http://localhost:8501\033[0m"
+    ;;
+
+  stop)
+    echo "Stopping docker containers..."
+    docker compose down
+    ;;
+
+  test)
+    echo "Running tests..."
+    echo "Running E2E-Tests..."
+    pushd e2e-tests
+    ./npmw ci
+    ./npmw run install
+    ./npmw run test
+    popd
+    ;;
+
+  *)
+    echo "Usage: $0 <command>"
+    echo ""
+    echo "Commands:"
+    echo "  start   Start the Docker services and wait for Ollama to be ready"
+    echo "  stop    Stop all running Docker services"
+    exit 1
+    ;;
+esac

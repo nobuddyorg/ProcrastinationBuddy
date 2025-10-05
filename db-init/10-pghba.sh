6a986a1c6c364db251e1cbaf1f6b8d31075dc17e
Matthias Eggert <dem.matze@gmail.com>
2025-10-05 18:39:18 +0200

    remove init script again

--- db-init/10-pghba.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-EXISTS=$(psql -U "$POSTGRES_USER" -d postgres -At -c "select 1 from pg_database where datname = 'tasks'")
-if [[ "$EXISTS" != "1" ]]; then
-  psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d postgres -c "create database tasks owner \"$POSTGRES_USER\";"
-fi

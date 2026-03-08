d10270f8ed5c3323de51b7b45bb4e8f98254b2d9
Matthias Eggert <dem.matze@gmail.com>
2026-03-08 19:07:23 +0100

    change ci behavior

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   testDir: "./tests",
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  retries: 1,
+  retries: process.env.CI ? 3 : 0,
   workers: 1,
   reporter: [
     ["junit", { outputFile: "test-results/results.xml" }],
@@ -14,11 +14,12 @@ export default defineConfig({
   timeout: 600_000,
   expect: { timeout: 10_000 },
   use: {
-    actionTimeout: 10_000,
+    actionTimeout: process.env.CI ? 20_000 : 10_000,
+    navigationTimeout: process.env.CI ? 60_000 : 30_000,
     trace: "retain-on-failure",
     screenshot: "only-on-failure",
     launchOptions: {
-      slowMo: 1000,
+      slowMo: process.env.CI ? 2000 : 200,
     },
   },
 
